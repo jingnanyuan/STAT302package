8b065fab26e88b0f0812c41d957f2f86d2f45d08
Jingnan Yuan <jingny@uw.edu>
2020-03-06 14:32:13 -0800

    Set up my_t.test function

---
+++ R/my_t.test.R
@@ -0,0 +1,37 @@
+#'T-test function
+#'
+#'This function performs a one sample t-test
+#'
+#'@param x Numberic vector of data.
+#'@param alternative Character string specifying the alternative hypothesis.
+#'  Only accept \code{"two.sided"}, \code{"less"}, or \code{"greater"}.
+#'@param mu Numberic input indicating the null hypothesis value of the mean.
+#'
+#'@return List with elements:
+#'  \code{test_stat}: the numeric test statistic.
+#'  \code{df}: the degrees of freedom.
+#'  \code{alternative}: the value of the parameter alternative.
+#'  \code{p_val}: the numeric p-value.
+#'
+#'@examples
+#'my_t.test(x, "two.sided", 12)
+#'
+#'@export
+my_t.test <- function(x, alternative, mu) {
+  mu_hat <- mean(x)
+  se <- sd(x)/sqrt(length(x))
+  test_stat <- (mu_hat - mu)/se
+  df <- length(x)-1
+  #calculate different p-value according different hypothesis
+  if (!(alternative %in% c("two.sided", "less", "greater"))) {
+    stop("Must input a valid althernative pyhothesis!")
+  } else if (alternative == c("two.sided")){
+    p_val <- 2*pt(test_stat, df, lower.tail = TRUE)
+  } else if (alternative == c("less")) {
+    p_val <- pt(test_stat, df, lower.tail = TRUE)
+  } else { #alternative == c("greater")
+    p_val <- pt(test_stat, df, lower.tail = FALSE)
+  }
+  my_list <- list("test_stat" = test_stat, "df" = df, "alternative" = alternative, "p_value" = p_val)
+  return(my_list)
+}
