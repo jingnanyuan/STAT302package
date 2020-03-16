b58423b91295a44e13f2ee15c36bd35dd1c9b0b6
Jingnan Yuan <jingny@uw.edu>
2020-03-16 14:27:37 -0700

    Document data set

---
+++ R/my_gapminder.R
@@ -0,0 +1,20 @@
+#' Gapminder Data
+#'
+#' Excerpt of the Gapminder data on life expectancy, GDP per capita,
+#'   and population by country.
+#'
+#' @format The main data frame gapminder has 1704 rows and 6 variables:
+#' \describe{
+#' \item{country}{factor with 142 levels}
+#' \item{continent}{factor with 5 levels}
+#' \item{year}{ranges from 1952 to 2007 in increments of 5 years}
+#' \item{lifeExp}{life expectancy at birth, in years}
+#' \item{pop}{population}
+#' \item{gdpPercap}{GDP per capita (US$, inflation-adjusted)}
+#' }
+#' The supplemental data frame gapminder_unfiltered was not filtered on \code{year}
+#'   or for complete data and has 3313 rows.
+#'
+#' @source
+#' http://www.gapminder.org/data/
+"my_gapminder"
