cd947fbf4b17f11c7e49343f614e747dbf2f37e0
Jingnan Yuan <jingny@uw.edu>
2020-03-17 01:12:20 -0700

    Change data set

--- R/my_rf_cv.R
+++ R/my_rf_cv.R
@@ -14,18 +14,16 @@
 #'
 #' @export
 my_rf_cv <- function(k) {
-  n <- nrow(my_iris)
+  n <- nrow(my_gapminder)
   fold <- sample(rep(1:k, length = n))
   mse <- rep(NA, k)
   #train the model through every fold
   for (i in 1:k) {
-    data_train <- my_iris[fold != i, ]
-    data_test <- my_iris[fold == i, ]
-    model <- randomForest(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width, data = data_train, ntree = 100)
-    pred <- predict(model, data_test[, -1])
-    #the average squared difference between
-    #predicted Sepal.Length and true Sepal.Length
-    mse[i] <- mean((pred - data_test$Sepal.Length)^2)
+    data_train <- my_gapminder[fold != i, ]
+    data_test <- my_gapminder[fold == i, ]
+    model <- randomForest(lifeExp ~ gdpPercap , data = data_train, ntree = 100)
+    pred <- predict(model, data_test[, 6])
+    mse[i] <- mean((pred - data_test$lifeExp)^2)
   }
   output <- mean(mse)
   return(output)
