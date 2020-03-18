ad0fc05e6f60660958f7fec710083ff2a90e9bef
Jingnan Yuan <jingny@uw.edu>
2020-03-18 03:26:08 -0700

    Improve the test

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -1,3 +1,5 @@
+library(STAT302package)
+
 lm_test <- my_lm(lifeExp ~ gdpPercap + continent, my_gapminder)
 
 test_that("my_lm works", {
@@ -7,4 +9,8 @@ test_that("my_lm works", {
   expect_equal(lm_test[3,2], c(0.6), tolerance = .001)
   expect_equal(lm_test[5,3], c(28.1), tolerance = .001)
   expect_equal(lm_test[5,4], c(0), tolerance = .0001)
+})
+
+test_that("wrong input throws error", {
+  expect_error(my_lm(lifeExp ~ gdpPercap + continent, 7))
 })
