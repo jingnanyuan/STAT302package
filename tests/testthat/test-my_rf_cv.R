83e06e6432dea3707a0c7de4cfe5e562b48c428c
Jingnan Yuan <jingny@uw.edu>
2020-03-18 04:16:45 -0700

    Add failure test

--- tests/testthat/test-my_rf_cv.R
+++ tests/testthat/test-my_rf_cv.R
@@ -1,3 +1,9 @@
+library(STAT302package)
+
 test_that("my_rf_cv works", {
-  expect_equal(my_rf_cv(5), 75.73032, tolerance = .0001)
+  expect_equal(my_rf_cv(5), 76, tolerance = .5)
+})
+
+test_that("wrong input throws error", {
+  expect_error(my_rf_cv("a string"))
 })
