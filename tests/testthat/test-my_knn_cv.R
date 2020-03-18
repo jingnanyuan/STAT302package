a2f9ac1be305d96a36620c213bfd409a2b27a471
Jingnan Yuan <jingny@uw.edu>
2020-03-18 04:11:22 -0700

    Improve the test

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -1,7 +1,12 @@
-set.seed(302)
+library(STAT302package)
 
 knn_test <- my_knn_cv(my_iris[, -5], my_iris$Species, 5, 5)
 
 test_that("my_knn_cv works", {
   expect_equal(knn_test[[2]], c(0), tolerance = .05)
+})
+
+test_that("wrong input throws error", {
+  expect_error(my_knn_cv(my_iris[, 8], my_iris$Species, 5, 5))
+  expect_error(my_knn_cv(my_iris[, 8], my_iris$Species, 0, 5))
 })
